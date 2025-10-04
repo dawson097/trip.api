de5cd474bec2216a6034892e4008dad3090f254e
Dawson Huang <dawson09@163.com>
2025-10-04 20:31:21 +0800

    Return the correct content

--- src/Controllers/TouristRoutesController.cs
+++ src/Controllers/TouristRoutesController.cs
@@ -16,12 +16,26 @@ public class TouristRoutesController : ControllerBase
     [HttpGet]
     public IActionResult GetAllRoutes()
     {
-        return Ok(_routeRepository.GetAllRoutes());
+        var routesFromRepo = _routeRepository.GetAllRoutes();
+
+        if (routesFromRepo == null || !routesFromRepo.Any())
+        {
+            return NotFound("找不到任何旅游路线");
+        }
+
+        return Ok(routesFromRepo);
     }
 
     [HttpGet("{routeId:guid}")]
     public IActionResult GetRouteById(Guid routeId)
     {
-        return Ok(_routeRepository.GetRouteById(routeId));
+        var routeFromRepo = _routeRepository.GetRouteById(routeId);
+
+        if (routeFromRepo == null)
+        {
+            return NotFound($"路线id为({routeId})的旅游路线找不到");
+        }
+
+        return Ok(routeFromRepo);
     }
 }
