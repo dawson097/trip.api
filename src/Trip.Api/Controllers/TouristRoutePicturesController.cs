e21fdad45489e894bc2f395597f3cf39e322f9a5
Dawson Huang <dawson09@163.com>
2026-04-13 16:52:06 +0800

    feat: 创建旅游路线图片控制器路由，并实现查询所有旅游路线图片和查询单个旅游路线图片的方法

---
+++ src/Trip.Api/Controllers/TouristRoutePicturesController.cs
@@ -0,0 +1,58 @@
+using MapsterMapper;
+using Microsoft.AspNetCore.Mvc;
+using Trip.Api.Dtos.TouristRoutePicture;
+using Trip.Api.Services.Interfaces;
+
+namespace Trip.Api.Controllers;
+
+/// <summary>
+/// 旅游路线图片控制器路由
+/// </summary>
+[ApiController, Route("api/tourist-routes/{routeId:guid}/pictures")]
+public class TouristRoutePicturesController : ControllerBase
+{
+    private readonly IMapper _mapper;
+    private readonly ITouristRoutePictureRepository _pictureRepository;
+
+    public TouristRoutePicturesController(IMapper mapper, ITouristRoutePictureRepository pictureRepository)
+    {
+        _mapper = mapper;
+        _pictureRepository = pictureRepository;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetTouristRoutePicturesAsync([FromRoute] Guid routeId)
+    {
+        if (!await _pictureRepository.RoutesExitsAsync(routeId))
+        {
+            return NotFound($"旅游路线({routeId})找不到");
+        }
+
+        var picturesFromRepo = await _pictureRepository.GetAllPicturesByRouteIdAsync(routeId);
+
+        if (picturesFromRepo == null || !picturesFromRepo.Any())
+        {
+            return NotFound("找不到任何图片");
+        }
+
+        return Ok(_mapper.Map<IEnumerable<TouristRoutePictureDto>>(picturesFromRepo));
+    }
+
+    [HttpGet("{pictureId:int}")]
+    public async Task<IActionResult> GetTouristRoutePictureAsync([FromRoute] Guid routeId, [FromRoute] int pictureId)
+    {
+        if (!await _pictureRepository.RoutesExitsAsync(routeId))
+        {
+            return NotFound($"旅游路线({routeId})找不到");
+        }
+
+        var pictureFromRepo = await _pictureRepository.GetRouteByIdAsync(pictureId);
+
+        if (pictureFromRepo == null)
+        {
+            return NotFound($"图片({pictureId})找不到");
+        }
+
+        return Ok(_mapper.Map<TouristRoutePictureDto>(pictureFromRepo));
+    }
+}
