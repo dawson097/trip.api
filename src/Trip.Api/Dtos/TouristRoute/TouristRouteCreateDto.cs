e5777330a3a9b6e23f8aaf7d022f80850ed3bca2
Dawson Huang <dawson09@163.com>
2026-04-14 00:09:03 +0800

    feat: 创建旅游路线DTO基类和旅游路线更新DTO，并让旅游路线更新DTO和旅游路线创建DTO继承旅游路线DTO基类

--- src/Trip.Api/Dtos/TouristRoute/TouristRouteCreateDto.cs
+++ src/Trip.Api/Dtos/TouristRoute/TouristRouteCreateDto.cs
@@ -1,37 +1,7 @@
-using Trip.Api.Dtos.TouristRoutePicture;
-
 namespace Trip.Api.Dtos.TouristRoute;
 
 /// <summary>
 /// 旅游路线创建DTO
 /// </summary>
-public class TouristRouteCreateDto
-{
-    public string Title { get; set; } = string.Empty;
-
-    public string Description { get; set; } = string.Empty;
-
-    public decimal Price { get; set; }
-
-    public DateTime CreateTime { get; set; }
-
-    public DateTime? UpdateTime { get; set; }
-
-    public DateTime? DepartureTime { get; set; }
-
-    public string Features { get; set; } = string.Empty;
-
-    public string Fees { get; set; } = string.Empty;
-
-    public string Notes { get; set; } = string.Empty;
-
-    public double? Rating { get; set; }
-
-    public string TripType { get; set; } = string.Empty;
-
-    public string TripDays { get; set; } = string.Empty;
-
-    public string DepartureCity { get; set; } = string.Empty;
-
-    public ICollection<TouristRoutePictureDto> TouristRoutePictures { get; set; } = new List<TouristRoutePictureDto>();
-}
+public class TouristRouteCreateDto : TouristRouteManipulationDto
+{ }
---
+++ src/Trip.Api/Dtos/TouristRoute/TouristRouteUpdateDto.cs
@@ -0,0 +1,7 @@
+namespace Trip.Api.Dtos.TouristRoute;
+
+/// <summary>
+/// 旅游路线更新DTO
+/// </summary>
+public class TouristRouteUpdateDto : TouristRouteManipulationDto
+{ }
