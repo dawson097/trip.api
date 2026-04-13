b4bd8330041886ac416d77579bf551fcf2a8f398
Dawson Huang <dawson09@163.com>
2026-04-13 11:34:50 +0800

    feat: 创建公共仓储服务为仓储服务提供公共方法

---
+++ src/Trip.Api/Services/CommonRepository.cs
@@ -0,0 +1,6 @@
+using Trip.Api.Services.Interfaces;
+
+namespace Trip.Api.Services;
+
+public class CommonRepository : ICommonRepository
+{ }
---
+++ src/Trip.Api/Services/Interfaces/ICommonRepository.cs
@@ -0,0 +1,8 @@
+namespace Trip.Api.Services.Interfaces;
+
+/// <summary>
+/// 公共仓储服务
+/// </summary>
+/// <remarks>为仓储服务提供公共方法</remarks>
+public interface ICommonRepository
+{ }
