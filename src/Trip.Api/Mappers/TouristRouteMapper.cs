a15e5b0a06240088bbb34476b4557e3f7d3d1470
Dawson Huang <dawson09@163.com>
2026-04-13 17:32:19 +0800

    refactor: 完善旅游路线路线映射

--- src/Trip.Api/Mappers/TouristRouteMapper.cs
+++ src/Trip.Api/Mappers/TouristRouteMapper.cs
@@ -11,6 +11,10 @@ public class TouristRouteMapper : IRegister
 {
     public void Register(TypeAdapterConfig config)
     {
-        config.NewConfig<TouristRoute, TouristRouteDto>();
+        config.NewConfig<TouristRoute, TouristRouteDto>()
+            .Map(dest => dest.Price, src => src.OriginalPrice * (decimal)(src.DiscountPresent ?? 1))
+            .Map(dest => dest.TripType, src => src.TripType.ToString())
+            .Map(dest => dest.TripDays, src => src.TripDays.ToString())
+            .Map(dest => dest.DepartureCity, src => src.DepartureCity.ToString());
     }
 }
