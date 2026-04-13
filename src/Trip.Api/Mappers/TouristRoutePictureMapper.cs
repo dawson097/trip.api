f14eeffd68d04bd6262de2495c60b86327d293a6
Dawson Huang <dawson09@163.com>
2026-04-13 16:51:19 +0800

    feat: 创建旅游路线图片自动映射

---
+++ src/Trip.Api/Mappers/TouristRoutePictureMapper.cs
@@ -0,0 +1,16 @@
+using Mapster;
+using Trip.Api.Dtos.TouristRoutePicture;
+using Trip.Api.Entities;
+
+namespace Trip.Api.Mappers;
+
+/// <summary>
+/// 旅游路线图片映射
+/// </summary>
+public class TouristRoutePictureMapper : IRegister
+{
+    public void Register(TypeAdapterConfig config)
+    {
+        config.NewConfig<TouristRoutePicture, TouristRoutePictureDto>();
+    }
+}
