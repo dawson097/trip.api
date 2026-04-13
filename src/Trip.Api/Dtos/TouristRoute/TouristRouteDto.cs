f33240e21fc066fbb73e7d33b62fd0c91e78ca07
Dawson Huang <dawson09@163.com>
2026-04-13 16:20:17 +0800

    feat: 分离旅游路线实体和DTO

---
+++ src/Trip.Api/Dtos/TouristRoute/TouristRouteDto.cs
@@ -0,0 +1,35 @@
+namespace Trip.Api.Dtos.TouristRoute;
+
+/// <summary>
+/// 旅游路线DTO
+/// </summary>
+public class TouristRouteDto
+{
+    public Guid Id { get; set; }
+
+    public string Title { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public decimal Price { get; set; }
+
+    public DateTime CreateTime { get; set; }
+
+    public DateTime? UpdateTime { get; set; }
+
+    public DateTime? DepartureTime { get; set; }
+
+    public string Features { get; set; } = string.Empty;
+
+    public string Fees { get; set; } = string.Empty;
+
+    public string Notes { get; set; } = string.Empty;
+
+    public double? Rating { get; set; }
+
+    public string TripType { get; set; } = string.Empty;
+
+    public string TripDays { get; set; } = string.Empty;
+
+    public string DepartureCity { get; set; } = string.Empty;
+}
