74c039cd781d4ac1237377f664dc163a483513ff
Dawson Huang <dawson09@163.com>
2026-04-20 00:07:53 +0800

    feat: 创建分页参数处理器

---
+++ src/Trip.Api/ResourceParameters/PaginationResourceParameter.cs
@@ -0,0 +1,36 @@
+namespace Trip.Api.ResourceParameters;
+
+/// <summary>
+/// 分页参数处理
+/// </summary>
+public class PaginationResourceParameter
+{
+    private int _pageNumber = 1;
+    private int _pageSize = 10;
+
+    public int PageSize
+    {
+        get => _pageSize;
+        set
+        {
+            const int MaxPageSize = 100;
+
+            if (value >= 1)
+            {
+                _pageSize = value > MaxPageSize ? MaxPageSize : value;
+            }
+        }
+    }
+
+    public int PageNumber
+    {
+        get => _pageNumber;
+        set
+        {
+            if (value >= 1)
+            {
+                _pageNumber = value;
+            }
+        }
+    }
+}
